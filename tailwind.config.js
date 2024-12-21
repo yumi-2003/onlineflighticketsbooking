6f9c12ea7f3d287f8dcb5a4b6e410036ba52cfaa
Ingyin May <ingyinmay1932003@gmai.com>
2024-12-21 08:10:23 +0630

    updated files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js,php}"],
+  content: ["./src/*.{html,js,php}",'./node_modules/flowbite/**/*.js'],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require('flowbite/plugin')],
+};
 
